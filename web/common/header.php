f99dfefc00baa4392944ecdadf76fbaded5f2996
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-04 18:16:56 +0300

    add changes

--- web/common/header.php
+++ web/common/header.php
@@ -1,4 +1,4 @@
 <nav>
-<a href="\cs313-php\web\personal_homepage.php" title="a link to Home page">Home</a>
-<a href="\cs313-php\web\assignments_page.php" title="a link to Assignments page">Assignments</a>
+<a href="personal_homepage.php" title="a link to Home page">Home</a>
+<a href="assignments_page.php" title="a link to Assignments page">Assignments</a>
 </nav>
