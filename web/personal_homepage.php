63324374138fc2fe13994915b96f0e94ae21dbcf
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-04 18:40:45 +0300

    add changes

--- web/personal_homepage.php
+++ web/personal_homepage.php
@@ -8,7 +8,22 @@
  <header>
  <?php include $_SERVER[ 'DOCUMENT_ROOT' ].'/common/header.php'; ?>
  </header>
-  <p>Hello World</p>
+ <main>
+ <h1>Ukrainian Cousine</h1>
+ <h2>These are some of the dishes of the Ukrainian Cousine</h2>
+ <section>
+ <article>
+ <p></p>
+ <img src="/images/borsch-642964_1920.jpg">
+ </article>
+ <article>
+ <p></p>
+ <img src="/images/cooking-2132874_1920.jpg">
+ </article>
+ </section>
+ </main>
+
+  
   <footer>
   <?php include $_SERVER[ 'DOCUMENT_ROOT' ].'/common/footer.php'; ?>
   </footer>
