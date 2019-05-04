b73d01a8daeef16d3a3e40656de31ec20d22453b
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-04 18:05:24 +0300

    add changes

---
+++ web/assignments_page.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en-us">
+ <head>
+  <title>Assignments Page</title>
+  <link href="css/small.css" rel="stylesheet" media="screen">
+  <link href="css/medium.css" rel="stylesheet" media="screen">
+  <link href="css/large.css" rel="stylesheet" media="screen">
+ </head>
+ <body>
+ <header>
+ <?php include $_SERVER[ 'DOCUMENT_ROOT' ].'/common/header.php'; ?>
+ </header>
+  <p>Hello World</p>
+  <footer>
+  <?php include $_SERVER[ 'DOCUMENT_ROOT' ].'/common/footer.php'; ?>
+  </footer>
+ </body>
+</html>
