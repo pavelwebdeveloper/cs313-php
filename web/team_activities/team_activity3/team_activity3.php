9cc1843e11fbe40c970e13a735fbaa0b4e3812e3
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-09 02:07:47 +0300

    add

--- web/team_activities/team_activity3/team_activity3.php
+++ web/team_activities/team_activity3/team_activity3.php
@@ -15,6 +15,9 @@
 $majors = array("CS" => "Computer Science", "WDD" => "Web Design and Development", "CIT" => "Computer information Technology", 
 "CE" => "Computer Engineering"); 
 var_dump($majors);
+foreach($majors as $major) {
+	echo '<input type="radio" name="major" value="'.$major.'>'.$major.'<br>';
+}
 ?>
 <input type="radio" name="major" value="computer science">Computer Science
 <br>
