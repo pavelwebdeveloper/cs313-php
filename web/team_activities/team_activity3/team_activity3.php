d435417ef746cc1f01dd1da1bb2f556b285fdf57
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-08 08:45:26 +0300

    add changes

--- web/team_activities/team_activity3/team_activity3.php
+++ web/team_activities/team_activity3/team_activity3.php
@@ -9,13 +9,18 @@
 <input type="text" name="name"><br>
 <label for="email">E-mail:</label>
 <input type="text" name="email"><br>
-<label for="major">Major:</label>
+<label for="major">Major:</label><br>
 <input type="radio" name="major" value="computer science">Computer Science
+<br>
 <input type="radio" name="major" value="web design and development">Web Design and Development
+<br>
 <input type="radio" name="major" value="computer information technology">Computer information Technology
+<br>
 <input type="radio" name="major" value="computer engineering">Computer Engineering
+<br>
 <label for="comment">Comments:</label>
 <textarea name="comment" rows="10" cols="50"></textarea>
+<br>
 <input type="submit" value="Submit">
 </form>
 </body>
