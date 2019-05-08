5d0a1bbcb2506e67e8a41657a8d146fe18366187
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-09 02:50:24 +0300

    add

--- web/team_activities/team_activity3/process_formdata.php
+++ web/team_activities/team_activity3/process_formdata.php
@@ -10,7 +10,8 @@
 
 var_dump($_POST);
 // define variables and set to empty values
-$name = $email = $major = $comment = $continent1 = $continent2 = $continent3 = $continent4 = $continent5 = $continent6 = $continent7 = "";
+$name = $email = $major = $comment = "";
+//$continent1 = $continent2 = $continent3 = $continent4 = $continent5 = $continent6 = $continent7 = "";
 
 $continents = array("$_POST['continent1']" => "North America", "$_POST['continent2']" => "South America", "$_POST['continent3']" => "Europe", 
 "$_POST['continent4']" => "Asia", "$_POST['continent5']" => "Australia", "$_POST['continent6']" => "Africa", "$_POST['continent7']" => "Antarctica"); 
@@ -23,6 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$email = test_input($_POST["email"]);
 	$major = test_input($_POST["major"]);
 	$comment = test_input($_POST["comment"]);
+	/*
 	$continent1 = test_input($_POST["continent1"]);
 	$continent2 = test_input($_POST["continent2"]);
 	$continent3 = test_input($_POST["continent3"]);
@@ -30,6 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$continent5 = test_input($_POST["continent5"]);
 	$continent6 = test_input($_POST["continent6"]);
 	$continent7 = test_input($_POST["continent7"]);
+	*/
 }
 
 function test_input($data) {
@@ -49,9 +52,9 @@ echo "<h1>Sibmitted information</h1><br><br>";
 echo "Name: ".$name."<br>E-mail: ".$email."<br>Major: ".$major."<br>Comment: ".$comment."<br>";
 //echo "<h3>Visited continents:</h3>".$continent1." ".$continent2." ".$continent3." ".$continent4." ".$continent5." ".$continent6." ".$continent7;
 echo "<h3>Visited continents:</h3>";
-foreach($continents as $continent) {
+/*foreach($continents as $continent) {
 	echo $continent;
-}
+}*/
 
 /*
 echo "<label for='name'>Name: ".$name."</label><br><label for='email'>E-mail: "$email."</label><br><label for='major'>Major: ".$major."</label><br><label for='comment'>Comments: ".$comment."</label>";
