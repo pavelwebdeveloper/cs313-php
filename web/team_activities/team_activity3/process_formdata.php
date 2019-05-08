501dc571f3ec702f29b8c7ce782cd50bb21ea414
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-09 01:52:42 +0300

    add

--- web/team_activities/team_activity3/process_formdata.php
+++ web/team_activities/team_activity3/process_formdata.php
@@ -10,7 +10,7 @@
 
 var_dump($_POST);
 // define variables and set to empty values
-$name = $email = $major = $comment = $continent = "";
+$name = $email = $major = $comment = $continent1 = $continent2 = $continent3 = $continent4 = $continent5 = $continent6 = $continent7 = "";
 
 //var_dump($major);
 
@@ -20,6 +20,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$email = test_input($_POST["email"]);
 	$major = test_input($_POST["major"]);
 	$comment = test_input($_POST["comment"]);
+	$continent1 = test_input($_POST["continent1"]);
+	$continent2 = test_input($_POST["continent2"]);
+	$continent3 = test_input($_POST["continent3"]);
+	$continent4 = test_input($_POST["continent4"]);
+	$continent5 = test_input($_POST["continent5"]);
+	$continent6 = test_input($_POST["continent6"]);
+	$continent7 = test_input($_POST["continent7"]);
 }
 
 function test_input($data) {
@@ -37,6 +44,7 @@ var_dump($_SERVER["REQUEST_METHOD"]);
 <?php
 echo "<h1>Sibmitted information</h1><br><br>";
 echo "Name: ".$name."<br>E-mail: ".$email."<br>Major: ".$major."<br>Comment: ".$comment."<br>";
+echo "<h3>I have visited the following continents:</h3>".$continent1."".$continent2."".$continent3."".$continent4."".$continent5."".$continent6."".$continent7;
 
 /*
 echo "<label for='name'>Name: ".$name."</label><br><label for='email'>E-mail: "$email."</label><br><label for='major'>Major: ".$major."</label><br><label for='comment'>Comments: ".$comment."</label>";
