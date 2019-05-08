db83dca68a98d9a3a38fa9ba6ab9607e6693ac6d
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-08 09:17:00 +0300

    add changes

--- web/team_activities/team_activity3/team_activity3.php
+++ web/team_activities/team_activity3/team_activity3.php
@@ -4,7 +4,28 @@
 <title>Team Activity 2</title>
 </head>
 <body>
-<form method="post" action="process_formdata.php">
+
+<?php
+// define variables and set to empty values
+$name = $email = $major = $comment = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$name = test_input($_POST["name"]);
+	// Check if name contains only letters and whitespace
+	$name = test_input($_POST["email"]);
+	$name = test_input($_POST["major"]);
+	$name = test_input($_POST["comment"]);
+}
+
+function test_form_input($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+?>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 <label for="name">Name:</label>
 <input type="text" name="name"><br>
 <label for="email">E-mail:</label>
@@ -23,5 +44,16 @@
 <br>
 <input type="submit" value="Submit">
 </form>
+
+<?php 
+echo "<label for="name">Name: ".$name."</label>
+<br>
+<label for="email">E-mail: "$email."</label>
+<br>
+<label for="major">Major: ".$major."</label>
+<br>
+<label for="comment">Comments: ".$comment."</label>"
+?>
+
 </body>
 </html>
