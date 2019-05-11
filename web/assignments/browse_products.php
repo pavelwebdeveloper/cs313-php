d00db0cd6d6026af40b5562a2dfb918b1b69dc3a
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-11 11:59:32 +0300

    add

--- web/assignments/browse_products.php
+++ web/assignments/browse_products.php
@@ -15,7 +15,44 @@ session_start();
  <main>
  <?php
  var_dump($_SESSION);
+ 
+ // Create an array for the shopping cart in the session
+ $_SESSION['shoppingCart'] = array();
+ 
+ // Add the 1st product
+ //$_SESSION['shoppingCart']['product1'] = array();
+ 
+ // Initialize the 1st product values
+ // Add the 1st product
+ //$_SESSION['shoppingCart']['product1']['name'] = "";
+ //$_SESSION['shoppingCart']['product1']['img'] = "product_images/cellphone-cellular-device-50684.jpg";
+ //$_SESSION['shoppingCart']['product1']['description'] = "Full Screen Unlocked";
+ 
+ 
+ $products = array(
+ array(
+ "title" => "Smartphone",
+ "image" => "product_images/cellphone-cellular-device-50684.jpg",
+ "price" => 500,
+ "description" => "Full Screen Unlocked Smartphone|5.7 Android Dual SIM Cell Phones, 512 RAM/512 ROM, GSM 2G",
+ "Stock" => 10
+ ),
+ array(
+ "title" => "Watch",
+ "image" => "product_images/blur-brass-bronze-2113994.jpg",
+ "price" => 100,
+ "description" => "Original Mens Watch Analog Watch Dial, Pro Sport Diver with Screw Down Crown and Water Resistant to 200M",
+ "Stock" => 20
+ )
+ );
+ 
+ foreach ($products as $product) {
+	echo "<section><h2>".$product["title"]."</h2><article><div><img src=".$product["image"]."></div><div><p>".$product["price"].
+	"</p><p>".$product["description"]."</p></div></article></section>";
+ };
+ 
  ?>
+ 
  </main>
 
   
