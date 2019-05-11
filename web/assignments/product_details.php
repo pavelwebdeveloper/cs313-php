e028ed6780e9f11b193aafdccaa6c82815ef460f
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-11 17:46:53 +0300

    add

--- web/assignments/product_details.php
+++ web/assignments/product_details.php
@@ -68,11 +68,11 @@ session_start();
  
  function copyProduct(){
 	global $product;
- static $product["title"] = $_POST["title"];
- static $product["image"] = $_POST["image"];
- static $product["price"] = $_POST["price"];
- static $product["description"] = $_POST["description"];
- static $product["stock"] = $_POST["stock"];
+ $product["title"] = $_POST["title"];
+ $product["image"] = $_POST["image"];
+ $product["price"] = $_POST["price"];
+ $product["description"] = $_POST["description"];
+ $product["stock"] = $_POST["stock"];
  }
  
  copyProduct();
