bea7342cd1ca165a26870fda91dcf7291d80ea70
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-11 15:29:34 +0300

    add

--- web/assignments/product_details.php
+++ web/assignments/product_details.php
@@ -62,6 +62,12 @@ session_start();
  );
  */
 
+ $product = array();
+ 
+ echo "<br>";
+ 
+ array_push($product, $_POST["title"], $_POST["image"], $_POST["price"], $_POST["description"], $_POST["stock"]); 
+ 
  
  foreach ($product as $item) {
 	echo "<section><h2>".$item["title"]."</h2><article><div><img src=".$item["image"]."></div><div><p class='price'><span>Price: </span>".$item["price"].
