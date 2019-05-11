9a27ba0db5d39c53cb7b094be97a8b5d2fdf73e1
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-12 02:26:54 +0300

    h

--- web/assignments/product_details.php
+++ web/assignments/product_details.php
@@ -119,11 +119,11 @@ session_start();
 		if ($phone != 0 || $watch != 0 || $binocular != 0) {
 		$_SESSION['shoppingCart'][] = $_SESSION['products'][0][$productNumber - 1];
 	}
-		if ($_SESSION['products'][0][$productNumber - 1]['numberOfProduct'] == 1) {
+		if ($_SESSION['products'][0][$productNumber - 1]['numberOfProduct'] = 1 && $_SESSION['products'][0][$productNumber - 1]['addedToCart'] == 1) {
 			$phone = 0;
-		} elseif ($_SESSION['products'][0][$productNumber - 1]['numberOfProduct'] == 2) {
+		} elseif ($_SESSION['products'][0][$productNumber - 1]['numberOfProduct'] = 2 && $_SESSION['products'][0][$productNumber - 1]['addedToCart'] == 1) {
 			$watch = 0;
-		} elseif ($_SESSION['products'][0][$productNumber - 1]['numberOfProduct'] == 3) {
+		} elseif ($_SESSION['products'][0][$productNumber - 1]['numberOfProduct'] = 3 && $_SESSION['products'][0][$productNumber - 1]['addedToCart'] == 1) {
 			$binocular = 0;
 		}
 
