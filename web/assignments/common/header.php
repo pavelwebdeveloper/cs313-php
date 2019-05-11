155a7ea228146d5188ce485de38cbd5f250a4c94
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-11 23:31:54 +0300

    g

--- web/assignments/common/header.php
+++ web/assignments/common/header.php
@@ -3,5 +3,5 @@
 <a href="browse_products.php" title="a link to Browse Products page">Products</a>
 </nav>
 <div>
-<a href="" title="a link to Shopping Cart page">Shopping Cart</a>
+<a href="view_cart.php" title="a link to Shopping Cart page">Shopping Cart</a>
 </div>
---
+++ web/assignments/view_cart.php
@@ -0,0 +1,108 @@
+<?php
+// Start the session
+session_start();
+// Create an array for the shopping cart in the session
+if (!isset($_SESSION['shoppingCart'])) {
+ $_SESSION['shoppingCart'] = array();
+ $_SESSION['products'] = array();
+ }
+?>
+<!DOCTYPE html>
+<html lang="en-us">
+ <head>
+  <title>View Cart Page</title>
+  <link href="css/online_store_styles.css" rel="stylesheet" media="screen">
+ </head>
+ <body>
+ <header>
+ <?php include $_SERVER[ 'DOCUMENT_ROOT' ].'/assignments/common/header.php'; ?>
+ </header>
+ <main>
+ <?php
+ echo "<br><h1>1</h1>";
+ var_dump($_SESSION);
+ echo "<br>";
+ echo "<br>";
+ echo "<br>";
+ 
+ 
+ echo "<br>";
+ 
+ echo "<br><h1>2</h1>";
+ var_dump($_SESSION);
+ echo "<br>";
+ echo "<br>";
+ echo "<br>";
+ 
+ // Add the 1st product
+ //$_SESSION['shoppingCart']['product1'] = array();
+ 
+ // Initialize the 1st product values
+ // Add the 1st product
+ //$_SESSION['shoppingCart']['product1']['name'] = "";
+ //$_SESSION['shoppingCart']['product1']['img'] = "product_images/cellphone-cellular-device-50684.jpg";
+ //$_SESSION['shoppingCart']['product1']['description'] = "Full Screen Unlocked";
+ /*
+ if (!isset($products)) {
+ $products = array(
+ array(
+ "numberOfProduct" => 1,
+ "title" => "Smartphone",
+ "image" => "product_images/cellphone-cellular-device-50684.jpg",
+ "price" => 500,
+ "description" => "Full Screen Unlocked Smartphone|5.7 Android Dual SIM Cell Phones, 512 RAM/512 ROM, GSM 2G",
+ "stock" => 10
+ ),
+ array(
+ "numberOfProduct" => 2,
+ "title" => "Watch",
+ "image" => "product_images/blur-brass-bronze-2113994.jpg",
+ "price" => 100,
+ "description" => "Original Mens Watch Analog Watch Dial, Pro Sport Diver with Screw Down Crown and Water Resistant to 200M",
+ "stock" => 30
+ ),
+ array(
+ "numberOfProduct" => 3,
+ "title" => "Binoculars",
+ "image" => "product_images/binoculars-black-equipment-55804.jpg",
+ "price" => 150,
+ "description" => "Black|25x magnifucation porro prism binocular|50mm objective lens|ultra sharp focus across the field of view| suitable for astronomical viewing|protective rubber covering",
+ "stock" => 20
+ )
+ );
+ }
+ */
+ /*
+ if (!isset($_SESSION['products'][0])) {
+  $_SESSION['products'][] = $products;
+ }*/
+ 
+ echo "<br><h1>3</h1>";
+ var_dump($_SESSION);
+ echo "<br>";
+ echo "<br>";
+ echo "<br>";
+ 
+ 
+ 
+ foreach ($_SESSION['shoppingCart'][] as $product) {
+	echo '<section><h2>'.$product["title"].'</h2><article><div><img src='.$product["image"].'></div><div><p class="price"><span>Price: </span>'.$product["price"].
+	'</p><p><span>Description: </span>'.$product["description"].'</p><p><span>Stock: </span>'.$product["stock"].
+	'</p><form method="post" action="product_details.php"><input type="hidden" name="title" value="'.$product["title"].
+	'"><input type="hidden" name="image" value="'.$product["image"].'"><input type="hidden" name="price" value="'.$product["price"].
+	'"><input type="hidden" name="description" value="'.$product["description"].'"><input type="hidden" name="stock" value="'.$product["stock"].
+	'"><input type="hidden" name="number" value="'.$product["numberOfProduct"].'"><input type="submit" name="numberOfProduct" value="Product details"></form></div></article></section>';
+ };
+ 
+ ?>
+ 
+ 
+ 
+ </main>
+
+  
+  <footer>
+  <?php include $_SERVER[ 'DOCUMENT_ROOT' ].'/assignments/common/footer.php'; ?>
+  </footer>
+ </body>
+</html>
