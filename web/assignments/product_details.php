68a4e58598430593796e7f1b464a56294fcdb2f3
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-11 21:02:47 +0300

    h

--- web/assignments/product_details.php
+++ web/assignments/product_details.php
@@ -118,7 +118,7 @@ session_start();
 	echo "<br>";
 	echo "<br>";
 	echo "<br>";
-	var_dump($_SESSION['products'][0][$productNumber]);
+	var_dump($_SESSION['products'][0][$productNumber - 1]);
 	echo "<br>";
 	echo "<br>";
 	echo "<br>";
