41fa2db5bf3c061c66de073a8f9ea0f439b6eb19
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-11 12:46:14 +0300

    add

--- web/assignments/browse_products.php
+++ web/assignments/browse_products.php
@@ -39,20 +39,27 @@ session_start();
  "image" => "product_images/cellphone-cellular-device-50684.jpg",
  "price" => 500,
  "description" => "Full Screen Unlocked Smartphone|5.7 Android Dual SIM Cell Phones, 512 RAM/512 ROM, GSM 2G",
- "Stock" => 10
+ "stock" => 10
  ),
  array(
  "title" => "Watch",
  "image" => "product_images/blur-brass-bronze-2113994.jpg",
  "price" => 100,
  "description" => "Original Mens Watch Analog Watch Dial, Pro Sport Diver with Screw Down Crown and Water Resistant to 200M",
- "Stock" => 20
+ "stock" => 30
+ ),
+ array(
+ "title" => "Binoculars",
+ "image" => "product_images/binoculars-black-equipment-55804.jpg",
+ "price" => 150,
+ "description" => "Black|25x magnifucation porro prism binocular|50mm objective lens|ultra sharp focus across the field of view| suitable for astronomical viewing|protective rubber covering",
+ "stock" => 20
  )
  );
  
  foreach ($products as $product) {
 	echo "<section><h2>".$product["title"]."</h2><article><div><img src=".$product["image"]."></div><div><p class='price'>".$product["price"].
-	"</p><p>".$product["description"]."</p></div></article></section>";
+	"</p><p>".$product["description"]."</p><p>".$product["stock"]."</p></div></article></section>";
  };
  
  ?>
