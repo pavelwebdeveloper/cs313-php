1d5bb6778f81e50cd398243ce8fb80958a6297d2
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-12 03:05:18 +0300

    g

--- web/assignments/product_details.php
+++ web/assignments/product_details.php
@@ -109,21 +109,21 @@ session_start();
 		$product[] = $_SESSION['productNumber'];
 		$product[] = $_SESSION['image'];
 		$cars = array("BMW", "Mercedez");*/
-		//$_SESSION['products'][0][$productNumber - 1]['stock'] -= 1 ;
-		//$_SESSION['products'][0][$productNumber - 1]['addedToCart'] += 1 ;
+		$_SESSION['products'][0][$productNumber - 1]['stock'] -= 1 ;
+		$_SESSION['products'][0][$productNumber - 1]['addedToCart'] += 1 ;
 		
 		
 		
 		
-		if ($_SESSION['products'][0][$productNumber - 1]['addedToCart'] == 0) {
+		if ($_SESSION['products'][0][$productNumber - 1]['addedToCart'] == 1) {
 		$_SESSION['shoppingCart'][] = $_SESSION['products'][0][$productNumber - 1];
 	}
 		
 
 		$_SESSION['stock'] -= 1;;
 		$_SESSION['addedToCart'] += 1;;
-		$_SESSION['shoppingCart'][0][$productNumber - 1]['stock'] = $_SESSION['products'][0][$productNumber - 1]['stock'];
-		$_SESSION['shoppingCart'][0][$productNumber - 1]['addedToCart'] = $_SESSION['products'][0][$productNumber - 1]['addedToCart'];
+		//$_SESSION['shoppingCart'][0][$productNumber - 1]['stock'] -= 1;
+		//$_SESSION['shoppingCart'][0][$productNumber - 1]['addedToCart'] += 1;
 		//$_SESSION['stock'] -= 1;
 	}
 	
