6d333e67908eb8c1c4db000c9b42cc0d8fab1a17
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-12 03:02:22 +0300

    h

--- web/assignments/product_details.php
+++ web/assignments/product_details.php
@@ -109,13 +109,13 @@ session_start();
 		$product[] = $_SESSION['productNumber'];
 		$product[] = $_SESSION['image'];
 		$cars = array("BMW", "Mercedez");*/
-		$_SESSION['products'][0][$productNumber - 1]['stock'] -= 1 ;
-		$_SESSION['products'][0][$productNumber - 1]['addedToCart'] += 1 ;
+		//$_SESSION['products'][0][$productNumber - 1]['stock'] -= 1 ;
+		//$_SESSION['products'][0][$productNumber - 1]['addedToCart'] += 1 ;
 		
 		
 		
 		
-		if ($_SESSION['products'][0][$productNumber - 1]['addedToCart'] == 1) {
+		if ($_SESSION['products'][0][$productNumber - 1]['addedToCart'] == 0) {
 		$_SESSION['shoppingCart'][] = $_SESSION['products'][0][$productNumber - 1];
 	}
 		
