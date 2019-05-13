1a9ad6af55193581bd6f11d9cca5824d9e3bad92
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-13 23:05:18 +0300

    add

--- web/assignments/product_details.php
+++ web/assignments/product_details.php
@@ -70,22 +70,41 @@ session_start();
 	
 	if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['addToShoppingCart'])) {
 		
-		$_SESSION['products'][0][$productNumber - 1]['stock'] -= 1 ;
-		$_SESSION['products'][0][$productNumber - 1]['addedToCart'] += 1 ;
+		
 		/*
 		$quantity = count($_SESSION['shoppingCart']);
 		$i = $quantity - 1;
 		*/
 		
+		if(isset($_SESSION['shoppingCart'])) {
+			$numberOfProductItems = count($_SESSION['shoppingCart'][0])
+			foreach $_SESSION['shoppingCart'] as $product {
+				for($i = 0; $i < $numberOfProductItems; $i++) {
+					if ($_SESSION['shoppingCart'][$i]['numberOfProduct'] == $_SESSION['productNumber']) {
+						$_SESSION['products'][0][$productNumber - 1]['stock'] -= 1;
+						$_SESSION['products'][0][$productNumber - 1]['addedToCart'] += 1;
+						$_SESSION['shoppingCart'][$i]['stock'] -= 1;
+						$_SESSION['shoppingCart'][$i]['addedToCart'] += 1;
+						$addProduct = FALSE;
+						
+					}
+				}
+				if($addProduct) {
+					$_SESSION['shoppingCart'][] = $_SESSION['products'][0][$productNumber - 1];
+				}
+			}
+		} else {
+			$_SESSION['shoppingCart'][] = $_SESSION['products'][0][$productNumber - 1];
+		}
 		
 		
-		
+		/*
 		if ($_SESSION['products'][0][$productNumber - 1]['addedToCart'] == 1) {
 		$_SESSION['shoppingCart'][] = $_SESSION['products'][0][$productNumber - 1];
 	} else {
 		$_SESSION['shoppingCart'][$i]['stock'] -= 1;
 		$_SESSION['shoppingCart'][$i]['addedToCart'] += 1;
-	}
+	} */
 		
 
 		$_SESSION['stock'] -= 1;
