4f1522906a4bc93e04e9575b205aa4813dc755f9
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-22 02:06:40 +0300

    add

--- web/team_activities/team_activity5/team_activity5.php
+++ web/team_activities/team_activity5/team_activity5.php
@@ -33,12 +33,14 @@ var_dump($name);
 
 /*function findBooks($name) {*/
 $stmt = $db->prepare('SELECT * FROM Scriptures WHERE name=:name');
-// $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
 // $stmt->execute();
 // $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 /*$stmt->closeCursor();
  return $rows;  
 }*/
+echo "<br>";
+echo "<br>";
 var_dump($stmt);
 echo "<br>";
 echo "<br>";
