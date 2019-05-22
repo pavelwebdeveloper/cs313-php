8a0b66f1676a06f52564fde93670e0f89a4b6c00
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-22 10:36:46 +0300

    add

--- web/team_activities/team_activity5/team_activity5.php
+++ web/team_activities/team_activity5/team_activity5.php
@@ -33,17 +33,16 @@ $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
 echo "<br>";
 echo "<br>";
 var_dump($name);
-//$rows = findBooks($name);
+$rows = findBooks($name);
 
-/*function findBooks($name) {*/
+function findBooks($name) {
 
 $stmt = $db->prepare('SELECT * FROM Scriptures WHERE book=:name');
 $stmt->bindValue(':name', $name, PDO::PARAM_STR);
 $stmt->execute();
 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-/*$stmt->closeCursor();
- return $rows;  
-}*/
+return $rows;  
+}
 echo "<br>";
 echo "<br>";
 var_dump($stmt);
