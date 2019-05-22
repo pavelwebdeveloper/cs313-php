37f6b3b04d5779176f877dd22c3f59c45601ddee
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-22 23:46:03 +0300

    add

--- web/team_activities/team_activity5/scripture_details.php
+++ web/team_activities/team_activity5/scripture_details.php
@@ -11,7 +11,16 @@
  
  //include '../../library/connections.php';
  
+ $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
  
+ $stmt = $db->prepare('SELECT * FROM Scriptures WHERE id=:id');
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$stmt->execute();
+$scripture = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+if (isset($scripture)) {
+  echo '<p>' . $scripture['content'] . '</p><br><br>';
+}
 
 ?>
 
