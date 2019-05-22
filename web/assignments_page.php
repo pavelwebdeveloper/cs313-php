44c940ff2edebde5db0e371223c16aa564fc1340
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-23 01:26:06 +0300

    add

--- web/assignments_page.php
+++ web/assignments_page.php
@@ -9,10 +9,11 @@
  <?php include $_SERVER[ 'DOCUMENT_ROOT' ].'/common/header.php'; ?>
  </header>
   <main>
-  <h1 id="assignmentsheading">The results for team assignments</h1>
+  <h1 id="assignmentsheading">The results for assignments</h1>
   <br>
   <div id="activities">
   <div id="teamactivities">
+  <h2>The results for team assignments</h2>
   <a href="/team_activities/team_activity2/team_activity2.html">Team Activity 2</a>
   <br>
   <a href="/team_activities/team_activity3/team_activity3.php">Team Activity 3</a>
@@ -20,6 +21,7 @@
   <a href="/team_activities/team_activity5/team_activity5.php">Team Activity 5</a>
   </div>
   <div id="proveactivities">
+  <h2>The results for prove assignments</h2>
   <a href="/assignments/browse_products.php">Shopping Cart</a>
   <br>
   </div>
