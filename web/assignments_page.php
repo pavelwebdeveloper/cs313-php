8b54b7a27a20eefee26439f879fc6ef77234592b
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-23 01:33:10 +0300

    add

--- web/assignments_page.php
+++ web/assignments_page.php
@@ -13,17 +13,21 @@
   <br>
   <div id="activities">
   <div id="teamactivities">
-  <h2>The results for team assignments</h2>
-  <a href="/team_activities/team_activity2/team_activity2.html">Team Activity 2</a>
+  <h2 class="heading">The results for team assignments</h2>
+  <ul>
+  <li><a href="/team_activities/team_activity2/team_activity2.html">Team Activity 2</a></li>
   <br>
-  <a href="/team_activities/team_activity3/team_activity3.php">Team Activity 3</a>
+  <li><a href="/team_activities/team_activity3/team_activity3.php">Team Activity 3</a><li>
   <br>
-  <a href="/team_activities/team_activity5/team_activity5.php">Team Activity 5</a>
+  <li><a href="/team_activities/team_activity5/team_activity5.php">Team Activity 5</a></li>
+  </ul>
   </div>
   <div id="proveactivities">
-  <h2>The results for prove assignments</h2>
-  <a href="/assignments/browse_products.php">Shopping Cart</a>
+  <h2 class="heading">The results for prove assignments</h2>
+  <ul>
+  <li><a href="/assignments/browse_products.php">Shopping Cart</a></li>
   <br>
+  </ul>
   </div>
   </div>
   </main>
