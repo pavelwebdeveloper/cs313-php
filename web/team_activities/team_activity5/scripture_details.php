5977d01038039163e7f84ec0a5578ba511574395
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-23 00:09:36 +0300

    add

--- web/team_activities/team_activity5/scripture_details.php
+++ web/team_activities/team_activity5/scripture_details.php
@@ -13,23 +13,16 @@
  
  $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
  
- var_dump($id);
- echo "<br>";
- echo "<br>";
+ 
  
  $stmt = $db->prepare('SELECT content FROM Scriptures WHERE id=:id');
 $stmt->bindValue(':id', $id, PDO::PARAM_INT);
 $stmt->execute();
-var_dump($stmt);
-echo "<br>";
- echo "<br>";
 $scripture = $stmt->fetch(PDO::FETCH_ASSOC);
-var_dump($scripture);
-echo "<br>";
- echo "<br>";
+
 
 if (isset($scripture)) {
-  echo '<p>' . $scripture['content'] . '</p><br><br>';
+  echo '<p><b>' . $scripture['content'] . '</b></p><br><br>';
 }
 
 ?>
