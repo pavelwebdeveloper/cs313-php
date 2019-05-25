817cd126c50fbd3b4caf975ee1351a0232cc4a68
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-26 02:47:25 +0300

    add

--- web/assignments/home.php
+++ web/assignments/home.php
@@ -44,7 +44,7 @@ echo '</ul>';
    foreach ($db->query('SELECT * FROM product') as $product)
 {	
 echo '<section><h2>'.$product["product"].'</h2><article><div><img src='.$product["image"].'></div><div><p class="price"><span>Price: </span>'.$product["price"].
-	'</p><p><span>Description: </span>'.$product["description"].'</p><p><span>Stock: </span>'.$product["stock"].
+	'</p><p><span>Description: </span>'.$product["productdescription"].'</p><p><span>Stock: </span>'.$product["stock"].
 	'</p><form method="post" action="product_details.php"><input type="hidden" name="id" value="'.$product["id"].
 	'"><input type="submit" name="productDetails" value="Product details"></form></div></article></section>';
 }
