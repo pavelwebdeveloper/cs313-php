74628e49458be3a3540b397be60519309dde449f
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-26 04:54:46 +0300

    add

--- web/assignments/browse_products.php
+++ web/assignments/browse_products.php
@@ -181,7 +181,7 @@ if (!isset($_SESSION['shoppingCart'])) {
 	echo "<br>";
 	echo "<br>";
 	*/
-	var_dump($_SESSION['products'][0]);
+	var_dump($_SESSION['products'][1]);
 	echo "<br>";
 	echo "<br>";
 	echo "<br>";
