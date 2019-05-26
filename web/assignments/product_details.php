612a59a099881e908dd1d79ffe53c1e329760503
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-26 04:30:43 +0300

    add

--- web/assignments/product_details.php
+++ web/assignments/product_details.php
@@ -217,11 +217,13 @@ session_start();
 	echo "<br>";
 	echo "<br>";
 	echo "<br>";
-	var_dump($_SESSION['products'][0]);
+	*/
+	var_dump($_SESSION['products']);
 	echo "<br>";
 	echo "<br>";
 	echo "<br>";
 	var_dump($_SESSION['shoppingCart']);
+	/*
 	echo "<br>";
 	echo "<br>";
 	echo "<br>";
