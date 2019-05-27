7317f049a44f24dbcac36c25b53f40dba0349d07
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-28 02:30:39 +0300

    add

--- web/team_activities/team_activity6/process_data.php
+++ web/team_activities/team_activity6/process_data.php
@@ -57,6 +57,7 @@
   $rowsChanged = $stmt->rowCount()*/
  
  function insertIntoScriptures($book, $chapter, $verse, $content){
+	 global $db;
  $stmt = $db->prepare('INSERT INTO Scriptures (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)');
 $stmt->bindValue(':book', $book, PDO::PARAM_STR);
 $stmt->bindValue(':chapter', $chapter, PDO::PARAM_STR);
