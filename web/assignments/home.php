03283b783491f4ab3b23fcd7f8736b6cc7221393
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-05-27 14:47:25 +0300

    add

--- web/assignments/home.php
+++ web/assignments/home.php
@@ -41,12 +41,15 @@ echo '</ul>';
    <div id="flexlayoutright">
    <?php
    //$i = 1;
-   foreach ($db->query('SELECT * FROM product') as $product)
+   
+   $displayProductForHomePage = 21;
+   foreach ($db->query('SELECT * FROM product WHERE productgroupid = ' . $displayProductForHomePage . ';') as $product)
 {	
 echo '<section><h2>'.$product["product"].'</h2><article><div><img src='.$product["image"].'></div><div><p class="price"><span>Price: </span>'.$product["price"].
 	'</p><p><span>Description: </span>'.$product["productdescription"].'</p><p><span>Stock: </span>'.$product["stock"].
 	'</p><form method="post" action="product_details.php"><input type="hidden" name="id" value="'.$product["id"].
 	'"><input type="submit" name="productDetails" value="Product details"></form></div></article></section>';
+	$displayProductForHomePage += 1;
 }
    
    /*
