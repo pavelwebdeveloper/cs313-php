0dbdc37c20568e31a2b66a8c9d392307f170fcca
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-06-02 01:55:26 +0300

    add

--- web/assignments/update_account.php
+++ web/assignments/update_account.php
@@ -36,11 +36,13 @@ if (!isset($_SESSION['shoppingCart'])) {
 <input type="email" id="userEmail" name="userEmail" placeholder="someone@gmail.com" pattern="[a-z0-9._%+-]+@[a-z0-9.]+\.[a-z]{2,}$" <?php if(isset($_SESSION['userData']['email'])) {echo "value='$_SESSION['userData']['email']'"; } ?> required><br>
 <input type="submit" value="Update Account">
 <input type="hidden" name="updateAccount" value="updateAccount">
+<input type="hidden" name="userId" value="<?php if(isset($_SESSION['userData']['id'])){echo $_SESSION['userData']['id'];} ?>">
 </form>
 <h2>Change Password</h2>
    <p>You can use this form to update your password. Entering and submitting a new password in this field you will change the current password.</p>
 <form method="post" action="update_account.php">
 <label for="userPassword">Password:</label><br>
+<span class="passworddescription">Passwords must be at least 8 characters and contain at least 1 number, 1 capital letter, 1 lower case letter and 1 special character</span>
 <input type="password" name="userPassword" id="userPassword" pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required><br><br>
 <input type="submit" value="Change Password">
 <input type="hidden" name="updatePassword" value="updatePassword">
@@ -54,9 +56,10 @@ if(isset($_POST['updateAccount'])) {
 	// Filter and store the data
    $userName = filter_input(INPUT_POST, 'userName', FILTER_SANITIZE_STRING);
    $userEmail = filter_input(INPUT_POST, 'userEmail', FILTER_SANITIZE_EMAIL);
+   $userId = filter_input(INPUT_POST, 'userId', FILTER_SANITIZE_NUMBER_INT);
    
     // Check if the email address is different than the one in the session
-   if($clientEmail != $_SESSION['clientData']['clientEmail']) {
+   if($userEmail != $_SESSION['userData']['email']) {
      //  checking for an existing email address
    $alreadyexistingEmail = $db->prepare('SELECT email FROM storeuser WHERE email=:userEmail');
 $alreadyexistingEmail->bindValue(':userEmail', $userEmail, PDO::PARAM_STR);
@@ -77,49 +80,71 @@ $findEmail = $alreadyexistingEmail->fetch(PDO::FETCH_ASSOC);
    }
    }
    
-    var_dump($userName);
-	 echo "<br>";
-	  echo "<br>";
-	  var_dump($userEmail);
-	  echo "<br>";
-	  echo "<br>";
-	  var_dump($userPassword);
-	  echo "<br>";
-	  echo "<br>";
-	
+   // Check for missing data
+   if(empty($userName) || empty($userEmail)){
+    $_SESSION['message'] = '<p class="message">Please, provide information correctly for all form fields.</p>';
+    header("Location: update_account.php");
+    exit;
+   }
+  $updateAccount = $db->prepare('UPDATE storeuser SET username = :username, email = :useremail WHERE id = :userId'); 
+ $updateAccount->bindValue(':username', $userName, PDO::PARAM_STR);
+$updateAccount->bindValue(':useremail', $userEmail, PDO::PARAM_STR);
+ $updateAccount->bindValue(':userId', $userId, PDO::PARAM_INT);
+$updateAccount->execute();
+$updateAccountOutcome = $updateAccount;
    
-   // Hash the checked password
-   $hashedPassword = password_hash($userPassword, PASSWORD_DEFAULT);
-   var_dump($hashedPassword);
-	  echo "<br>";
-	  echo "<br>";
-   
-   
-   $stmt = $db->prepare('INSERT INTO storeuser (username, email, password, userlevel) VALUES (:username, :useremail, :userpassword, 1)'); 
- $stmt->bindValue(':username', $userName, PDO::PARAM_STR);
-$stmt->bindValue(':useremail', $userEmail, PDO::PARAM_STR);
- $stmt->bindValue(':userpassword', $hashedPassword, PDO::PARAM_STR);
-$stmt->execute();
-var_dump($stmt);
-	  echo "<br>";
-	  echo "<br>";
-$signUpOutcome = $stmt->rowCount();
-var_dump($signUpOutcome);
-	  echo "<br>";
-	  echo "<br>";
-   
-   // Check and report the result and create the cookie when the individual registers with the site
-   if($signUpOutcome === 1){
-    $_SESSION['message'] = "<p>Thanks for registering. Please, use your email and password to login.</p>";
-    header("Location: logIn_page.php");
+   // Check and report the result
+   if($updateAccountOutcome === 1){
+    // Query the client data based on the client ID
+   $getUserUpdatedData = $db->prepare('SELECT id, username, email FROM storeuser WHERE id=:userId');
+$getUserUpdatedData->bindValue(':userId', $userId, PDO::PARAM_INT);
+$getUserUpdatedData->execute();
+$userUpdatedData = $getUserUpdatedData->fetch(PDO::FETCH_ASSOC);
+   // Store the array into the session
+   $_SESSION['userData'] = $userUpdatedData;
+   // setcookie('firstname', $clientFirstname, strtotime('+1 year'), '/');
+    $_SESSION['message'] = "<p class='messagesuccess'>Your account information has been successfully updated.</p>";
+    header("Location: manage_account.php");
    exit;
    } else {
-    $message = "<p>Sorry, but the registration failed. Please, try again.</p>";
-	header("Location: signUp_page.php");
+    $message = "<p class='messagefailure'>Sorry, but the account update failed. Please, try again.</p>";
+            header("Location: manage_account.php");
     exit;
    }
    
+} elseif (isset($_POST['updatePassword'])) {
+	// Filter and store the password data
+   $userPassword = filter_input(INPUT_POST, 'userPassword', FILTER_SANITIZE_STRING);
+   $userId = filter_input(INPUT_POST, 'userId', FILTER_SANITIZE_NUMBER_INT);
+   
+   // Check for missing data
+   if(empty($userPassword)){
+    $_SESSION['message'] = '<p class="messagefailure">Please, provide the new password information correctly.</p>';
+    header("Location: update_account.php");
+    exit;
+   }
+   
+   // Hash the checked password
+   $hashedPassword = password_hash($userPassword, PASSWORD_DEFAULT);
+   
+   // Send the data to the model
+   $passwordUpdate = $db->prepare('UPDATE storeuser SET password = :password WHERE id = :userId'); 
+ $passwordUpdate->bindValue(':password', $hashedPassword, PDO::PARAM_STR);
+ $passwordUpdate->bindValue(':userId', $userId, PDO::PARAM_INT);
+$passwordUpdate->execute();
+$passwordUpdateOutcome = $passwordUpdate;
 }
+ // Check and report the result
+   if($passwordUpdateOutcome){
+    $_SESSION['message'] = "<p class='messagesuccess'>Your password has been successfully updated.</p>";
+    header("Location: manage_account.php");
+   exit;
+   } else {
+    $_SESSION['message'] = "<p>Sorry, but the password update failed. Please, try again.</p>";
+            header("Location: manage_account.php");
+    exit;
+   }
+
  ?>
  
  </main>
