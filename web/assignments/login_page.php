31f55e1c8b5e8f3d8925b3b0f2d99ff68a5b432e
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-06-03 09:28:37 +0300

    add

---
+++ web/assignments/login_page.php
@@ -0,0 +1,103 @@
+<?php
+// Start the session
+session_start();
+// Create an array for the shopping cart in the session
+if (!isset($_SESSION['shoppingCart'])) {
+ $_SESSION['shoppingCart'] = array();
+ $_SESSION['products'] = array();
+ }
+?>
+<!DOCTYPE html>
+<html lang="en-us" id="logInRegister">
+ <head>
+  <title>Home Page</title>
+  <link href="css/online_store_styles.css" rel="stylesheet" media="screen">
+  <link href="css/normalize.css" rel="stylesheet" media="screen">
+ </head>
+ <body>
+ 
+ <main>
+ 
+ <?php
+ // Get the database connection file
+ require_once '../library/connections.php';
+ ?>
+ 
+ 
+ <div>
+ <?php
+   if (isset($_SESSION['message'])) {
+    echo $_SESSION['message'];
+   }
+   ?>
+ <form method="post" action="logIn_page.php">
+<label for="userEmail">E-mail:</label><br>
+<input type="email" id="userEmail" name="userEmail" placeholder="someone@gmail.com" pattern="[a-z0-9\._%+-]+@[a-z0-9.]+\.[a-z]{2,}$" <?php if(isset($clientEmail)){echo "value='$clientEmail'";} ?> required><br>
+<label for="userPassword">Password:</label><br>
+<input type="password" name="userPassword" id="userPassword" pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required><br><br>
+<input class="submitButton" type="submit" value="Log in">
+<input type="hidden" name="LogIn" value="logIn">
+<br>
+<p id="login">Not registered yet?</p>
+<button type="button"><a id="aregister" href="signUp_page.php" title="a link to a sign_up page">Sign Up</a></button>
+</form>
+ </div>
+ 
+ <?php
+ 
+if(isset($_POST['LogIn'])) {
+	// Filter and store the data
+   $userEmail = filter_input(INPUT_POST, 'userEmail', FILTER_SANITIZE_EMAIL);
+   $userPassword = filter_input(INPUT_POST, 'userPassword', FILTER_SANITIZE_STRING);
+
+   
+   
+   // Check for missing data
+   if(empty($userEmail) || empty($userPassword)){
+    $_SESSION['message'] = "<p>Please, provide a valid email address and password.</p>";
+	header("Location: login_page.php");
+    exit;
+   }
+   
+   // Query the client data based on the email address
+   $getUserData = $db->prepare('SELECT id, username, email, password FROM storeuser WHERE email=:userEmail');
+$getUserData->bindValue(':userEmail', $userEmail, PDO::PARAM_STR);
+$getUserData->execute();
+$userData = $getUserData->fetch(PDO::FETCH_ASSOC);
+
+	  
+   
+   // Compare the password just submitted against
+   // the hashed password for the matching client
+   $hashCheck = password_verify($userPassword, $userData['password']);
+   
+	  
+   // If the hashes don't match create an error
+   // and return to the login view
+   if(!$hashCheck) {
+    $_SESSION['message'] = "<p>Please, check your password and try again.</p>";
+    header("Location: login_page.php");
+    exit;
+   }
+    // A valid user exists, log them in
+   $_SESSION['loggedin'] = TRUE;
+   // Remove the password from the array
+   // the array_pop function removes the last
+   // element from an array
+   array_pop($userData);
+   // Store the array into the session
+   $_SESSION['userData'] = $userData;
+   $_SESSION['message'] = '';
+   // Send them to the admin view
+   header("Location: home.php");
+   exit;
+   
+}
+
+ ?>
+ 
+ 
+ 
+ </main>
+ </body>
+</html>
---
+++ web/assignments/signup_page.php
@@ -0,0 +1,93 @@
+<?php
+// Start the session
+session_start();
+// Create an array for the shopping cart in the session
+if (!isset($_SESSION['shoppingCart'])) {
+ $_SESSION['shoppingCart'] = array();
+ $_SESSION['products'] = array();
+ }
+?>
+<!DOCTYPE html>
+<html lang="en-us" id="logInRegister">
+ <head>
+  <title>Home Page</title>
+  <link href="css/online_store_styles.css" rel="stylesheet" media="screen">
+  <link href="css/normalize.css" rel="stylesheet" media="screen">
+ </head>
+ <body>
+ 
+ <main>
+ 
+ <?php
+ // Get the database connection file
+ require_once '../library/connections.php';
+ ?>
+ 
+ <div>
+ <?php
+   if (isset($_SESSION['message'])) {
+    echo $_SESSION['message'];
+   }
+   ?>
+ <form method="post" action="signUp_page.php">
+<label for="userName">Full name:</label><br>
+<input type="text" id="userName" name="userName" pattern="[A-Za-z ]{2,}" required><br>
+<label for="userEmail">E-mail:</label><br>
+<input type="email" id="userEmail" name="userEmail" placeholder="someone@gmail.com" pattern="[a-z0-9._%+-]+@[a-z0-9.]+\.[a-z]{2,}$" required><br>
+<label for="userPassword">Password:</label><br>
+<span class="passworddescription">Passwords must be at least 8 characters and contain at least 1 number, 1 capital letter, 1 lower case letter and 1 special character</span>
+<input type="password" name="userPassword" id="userPassword" pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required><br><br>
+<input type="submit" value="Sign Up">
+<input type="hidden" name="SignUp" value="signUp">
+</form>
+ </div>
+ 
+ <?php
+ var_dump($_POST);
+  echo "<br>";
+if(isset($_POST['SignUp'])) {
+	// Filter and store the data
+   $userName = filter_input(INPUT_POST, 'userName', FILTER_SANITIZE_STRING);
+   $userEmail = filter_input(INPUT_POST, 'userEmail', FILTER_SANITIZE_EMAIL);
+   $userPassword = filter_input(INPUT_POST, 'userPassword', FILTER_SANITIZE_STRING);
+    // Check for missing data
+   if(empty($userName) || empty($userEmail) || empty($userPassword)){
+    $message = '<p>Please, provide information correctly for all form fields.</p>';
+	header("Location: signup_page.php");
+    exit;
+   }
+   
+       
+   // Hash the checked password
+   $hashedPassword = password_hash($userPassword, PASSWORD_DEFAULT);
+   var_dump($hashedPassword);
+	  echo "<br>";
+	  echo "<br>";
+   
+   
+   $stmt = $db->prepare('INSERT INTO storeuser (username, email, password, userlevel) VALUES (:username, :useremail, :userpassword, 1)'); 
+ $stmt->bindValue(':username', $userName, PDO::PARAM_STR);
+$stmt->bindValue(':useremail', $userEmail, PDO::PARAM_STR);
+ $stmt->bindValue(':userpassword', $hashedPassword, PDO::PARAM_STR);
+$stmt->execute();
+
+$signUpOutcome = $stmt->rowCount();
+
+   
+   // Check and report the result and create the cookie when the individual registers with the site
+   if($signUpOutcome === 1){
+    $_SESSION['message'] = "<p>Thanks for registering. Please, use your email and password to login.</p>";
+    header("Location: login_page.php");
+   exit;
+   } else {
+    $message = "<p>Sorry, but the registration failed. Please, try again.</p>";
+	header("Location: signup_page.php");
+    exit;
+   }
+   
+}
+ ?>
+ 
+ </main>
+ </body>
+</html>
