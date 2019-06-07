99b2096f7bcda9774b693d47a08f99a771020beb
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-06-07 08:59:40 +0300

    add

--- web/assignments/manage_account.php
+++ web/assignments/manage_account.php
@@ -37,6 +37,9 @@ if (!isset($_SESSION['shoppingCart'])) {
           
    echo  '<p>Your email: '.$_SESSION['userData']['email'].'<p>'
            ."<a class='adminlink' href='update_account.php?id=".urlencode($_SESSION['userData']['id'])."'>Update account information</a>";
+		   
+		   var_dump($_SESSION['userData']['userlevel']);
+		   
    if($_SESSION['userData']['userlevel'] > 1) {echo '<h2>As an administrator you can perform the following actions to manage the online store:</h2>'
    .'<a class="adminlink" href="manage_departmentgroup.php">Add or remove a department, product group</a><br>'
    .'<a class="adminlink" href="manage_products.php">Add, update or remove a product</a><br>';}
