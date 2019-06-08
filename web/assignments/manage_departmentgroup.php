6cd0d697fef6dad9f70af96e696929e46938f148
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-06-08 15:04:31 +0300

    add

--- web/assignments/manage_departmentgroup.php
+++ web/assignments/manage_departmentgroup.php
@@ -1,6 +1,5 @@
 <?php
-// Get the database connection file
- require_once '../library/connections.php';
+
 // Start the session
 session_start();
 if(!($_SESSION['loggedin'])){header('Location: home.php');}
@@ -10,6 +9,22 @@ if (!isset($_SESSION['shoppingCart'])) {
  $_SESSION['shoppingCart'] = array();
  $_SESSION['products'] = array();
 }
+?>
+<!DOCTYPE html>
+<html lang="en-us" id="logInRegister">
+ <head>
+  <title>Manage Product Department and Product Group Page</title>
+  <link href="css/online_store_styles.css" rel="stylesheet" media="screen">
+  <link href="css/normalize.css" rel="stylesheet" media="screen">
+ </head>
+ <body>
+ <header>
+ <?php include $_SERVER[ 'DOCUMENT_ROOT' ].'/assignments/common/header.php'; ?>
+ </header>
+ <main>
+ <?php
+ // Get the database connection file
+ require_once '../library/connections.php';
  // Query the product department data based on the email address
    $getDepartment = $db->prepare('SELECT * FROM productdepartment');
 $getDepartment->execute();
@@ -30,34 +45,50 @@ $departments = $getDepartment->fetchAll(PDO::FETCH_ASSOC);
   $departmentList .= ">$department[productdepartmentname]</option>";
  }
  $departmentList .= '</select>';
-?>
-<!DOCTYPE html>
-<html lang="en-us" id="logInRegister">
- <head>
-  <title>Manage Product Department and Product Group Page</title>
-  <link href="css/online_store_styles.css" rel="stylesheet" media="screen">
-  <link href="css/normalize.css" rel="stylesheet" media="screen">
- </head>
- <body>
- <header>
- <?php include $_SERVER[ 'DOCUMENT_ROOT' ].'/assignments/common/header.php'; ?>
- </header>
- <main>
+ ?>
  
+ <div>
  <?php
+   if (isset($_SESSION['message'])) {
+    echo $_SESSION['message'];
+   }
+   ?>
+   <form action="manage_departmentgroup.php" method="post">
+    <fieldset>
+	<legend>Add or remove product department</legend>
+     <label for="departmentName">Department Name</label>
+     <input type="text" name="departmentName" id="departmentName" pattern="[A-Z][a-z]{3,}" required><br>
+     <input class="submitBtn" type="submit" value="Add Department">
+     <!-- Add the action name - value pair -->
+     <input type="hidden" name="action" value="newDepartment">
+	 <input class="submitBtn" type="submit" value="Remove Department">
+     <!-- Add the action name - value pair -->
+     <input type="hidden" name="action" value="removeDepartment">
+    </fieldset>
+   </form>
+   
+   <form action="manage_departmentgroup.php" method="post">
+    <fieldset>
+	<legend>Add or remove product group</legend>
+	<?php
+	echo $departmentList;
+ ?>
+     <label for="productGroupName">Product Group Name</label>
+     <input type="text" name="productGroupName" id="productGroupName" pattern="[A-Z][a-z]{3,}" required><br>
+     <input class="submitBtn" type="submit" value="Add Product Group">
+     <!-- Add the action name - value pair -->
+     <input type="hidden" name="action" value="newProductGroup">
+	 <input class="submitBtn" type="submit" value="Remove Product Group">
+     <!-- Add the action name - value pair -->
+     <input type="hidden" name="action" value="removeProductGroup">
+    </fieldset>
+   </form>
+   </div>
+   
+   <?php
 if(isset($_POST['newDepartment'])) {
-	/*
-	var_dump($_POST);
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "Hi";
-*/
 	// Filter and store the data
 	$departmentName = filter_input(INPUT_POST, 'departmentName', FILTER_SANITIZE_STRING);
-	
-	
    
    // validate the categoryName variable using a custom function from functions.php
    $pattern = '/^[A-Z][a-z]{3,}$/';
@@ -72,10 +103,16 @@ echo "Hi";
    }
    
    $stmt = $db->prepare('INSERT INTO productdepartment (productdepartmentname) VALUES (:departmentName)'); 
- $stmt->bindValue(':departmentName', $checkedDepartmentName, PDO::PARAM_STR);
+ $stmt->bindValue(':departmentName', $departmentName, PDO::PARAM_STR);
 $stmt->execute();
-
-
+/*
+var_dump($stmt);
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "Hi";
+*/
    
    // Send the data to the model
    $adddepartmentOutcome = $stmt->rowCount();
@@ -93,66 +130,6 @@ $stmt->execute();
 }
 	
 	?>
- 
- <div>
- <?php
-   if (isset($_SESSION['message'])) {
-    echo $_SESSION['message'];
-   }
-   ?>
-   <form action="manage_departmentgroup.php" method="post">
-    <fieldset>
-	<legend>Add product department</legend>
-     <label for="departmentName">Department Name</label>
-     <input type="text" name="departmentName" id="departmentName" pattern="[A-Z][a-z]{3,}" required><br>
-     <input class="submitBtn" type="submit" value="Add Department">
-     <!-- Add the action name - value pair -->
-     <input type="hidden" name="action" value="newDepartment">	 
-    </fieldset>
-   </form>
-   
-   <form action="manage_departmentgroup.php" method="post">
-    <fieldset>
-	<legend>Remove product department</legend>
-     <label for="departmentName">Department Name</label>
-	 <?php
-	echo $departmentList;
- ?>
-	 <input class="submitBtn" type="submit" value="Remove Department">
-     <!-- Add the action name - value pair -->
-     <input type="hidden" name="action" value="removeDepartment">
-    </fieldset>
-   </form>
-   
-   <form action="manage_departmentgroup.php" method="post">
-    <fieldset>
-	<legend>Add product group</legend>
-	<?php
-	echo $departmentList;
- ?>
-     <label for="productGroupName">Product Group Name</label>
-     <input type="text" name="productGroupName" id="productGroupName" pattern="[A-Z][a-z]{3,}" required><br>
-     <input class="submitBtn" type="submit" value="Add Product Group">
-     <!-- Add the action name - value pair -->
-     <input type="hidden" name="action" value="newProductGroup">
-    </fieldset>
-   </form>
-   
-   <form action="manage_departmentgroup.php" method="post">
-    <fieldset>
-	<legend>Remove product group</legend>
-     <label for="productGroupName">Product Group Name</label>
-	 <?php
-	echo $departmentList;
- ?>
-	 <input class="submitBtn" type="submit" value="Remove Product Group">
-     <!-- Add the action name - value pair -->
-     <input type="hidden" name="action" value="removeProductGroup">
-    </fieldset>
-   </form>
-   </div>
-   
-   
    
  
    
