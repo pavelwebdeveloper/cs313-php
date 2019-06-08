bbd2f2524d5518456fd93683a681a6f900087ed1
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-06-08 22:28:26 +0300

    add

--- web/assignments/manage_departmentgroup.php
+++ web/assignments/manage_departmentgroup.php
@@ -356,6 +356,7 @@ echo "<br>";
    if($deleteProductGroupOutcome == 1){
 	   $_SESSION['message'] = "<p class='messagesuccess'>The product group " . $productGroupName['productgroupname'] . " has successfully been deleted.</p>";
 	   header('location: manage_departmentgroup.php');
+	   header('location: manage_departmentgroup.php');
    exit;
    } else {
     $_SESSION['message'] = "<p class='messagefailure'>Sorry, deleting the product group " . $productGroupName['productgroupname'] . " has failed. Please, try again.</p>";
