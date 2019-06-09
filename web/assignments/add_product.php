9ca1fc86264ed9b85063791f13720817d80dc66c
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-06-09 04:36:20 +0300

    add

--- web/assignments/add_product.php
+++ web/assignments/add_product.php
@@ -79,7 +79,7 @@ echo "<br>";
 	 <input type="number" name="productStock" id="productStock"><br><br>
 	 <input class="submitBtn" type="submit" value="Add product">
      <!-- Add the action name - value pair -->
-	 <input type="hidden" name="productDepartmentId" value="$_POST['departmentId']">
+	 <input type="hidden" name="departmentId" value="$_POST['departmentId']">
      <input type="hidden" name="AddProduct" value="addProduct">
     </fieldset>
    </form>
