586692b50eb602028c91e908d75647b0410b922b
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-06-11 06:19:34 +0300

    add

--- web/assignments/add_product.php
+++ web/assignments/add_product.php
@@ -28,7 +28,7 @@ if (!isset($_SESSION['shoppingCart'])) {
  // Query the product groups data
  
 
-if(!isset($_POST["departmentId"] && $_POST["AddProduct"])) {
+if(!isset($_POST["departmentId"]) && !isset($_POST["productName"])) {
 	 $_SESSION['message'] = "<p class='messagefailure'>Please, choose department in which you want to add a product.</p>";
    header('location: manage_products.php');
    exit;
