6fd48234cc1df23debdae9228ac67d43f87b45b1
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-09-11 00:40:54 +0300

    add

--- web/assignments/check_out.php
+++ web/assignments/check_out.php
@@ -53,19 +53,26 @@ if (!isset($_SESSION['shoppingCart'])) {
  
  <h1>This is Checkout Page</h1>
  <p>Please, input the information about your address. It is required to fill out all the fields.</p>
- <form method="post" <?php if(isset($_POST['CompletePurchase'])){"action='confirmation_page.php'";} else {"action='view_cart.php'";}?> >
+ <form method="post" <?php 
+if($_REQUEST['btn_submit']=="Complete the purchase")
+{
+action="confirmation_page.php";
+} else {
+action="view_cart.php";	
+}
+?> >
 <label class="address" for="country">Country: <input type="text" name="country" <?php if(isset($customerCountry)){echo "value='$customerCountry'";} else {echo "value=''";} ?> required></label><br>
 <label class="address" for="city">City: <input type="text" name="city" <?php if(isset($customerCity)){echo "value='$customerCity'";} else {echo "value=''";} ?> required></label><br>
 <label class="address" for="street">Street: <input type="text" name="street" <?php if(isset($customerStreet)){echo "value='$customerStreet'";} else {echo "value=''";} ?> required></label><br>
 <label class="address" for="houseNumber">House number: <input type="text" name="houseNumber" <?php if(isset($customerHouseNumber)){echo "value='$customerHouseNumber'";} else {echo "value=''";} ?> required></label><br>
 <label class="address" for="zipCode">Zipcode: <input type="text" name="zipCode" <?php if(isset($customerZipCode)){echo "value='$customerZipCode'";} else {echo "value=''";} ?> required></label><br>
 
-<input class="submitBtn" type="submit" name="CompletePurchase" value="Complete the purchase">
+<input class="submitBtn" type="submit" name="btn_submit" value="Complete the purchase">
 
 
 <div class="bottomNavigationLinks">
  <div>
- <input class="navigationButton" type="submit" name="ReturnToShoppingCart" value="Return to the Shopping Cart">
+ <input class="navigationButton" type="submit" name="btn_submit" value="Return to the Shopping Cart">
 </div>
 </div>
 
